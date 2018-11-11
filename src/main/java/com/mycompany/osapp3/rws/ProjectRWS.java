d64204d7b545768ee228ea6b2a82569859493ac5
Nuno Leite de Faria <nleitefaria@gmail.com>
2018-11-11 00:20:50 +0000

    Added findAll to Project

--- src/main/java/com/mycompany/osapp3/rws/ProjectRWS.java
+++ src/main/java/com/mycompany/osapp3/rws/ProjectRWS.java
@@ -1,5 +1,7 @@
 package com.mycompany.osapp3.rws;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mycompany.osapp3.entity.Project;
 import com.mycompany.osapp3.service.ProjectService;
+import com.mycompany.osapp4.entity.Countries;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000", "https://nleitefaria.github.io/ghp-app3"}) 
@@ -26,6 +30,12 @@ public class ProjectRWS
 	public ResponseEntity<Long> count() {
 		logger.info("@count");
 		return new ResponseEntity<Long>(service.count(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/projects", method = RequestMethod.GET)
+	public ResponseEntity<List<Project>> findAll()
+	{		
+		return new ResponseEntity<List<Project>>(service.findAll(), HttpStatus.OK);
 	}
 
 }
